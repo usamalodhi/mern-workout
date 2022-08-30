705fadcaee6a48c9dd40dfc2863aba61c01a9da7
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-08-30 17:01:49 +0100

    Update POST workout request to send new workout based off workoutModel

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 
 const router = express.Router();
 
@@ -13,9 +14,16 @@ router.get('/:id', (req, res) => {
   res.json({ mssg: 'GET a single workout' });
 });
 
-//POST a new workout
-router.post('/', (req, res) => {
-  res.json({ mssg: 'POST a new workout' });
+// POST a new workout
+router.post('/', async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //DELETE a workout
