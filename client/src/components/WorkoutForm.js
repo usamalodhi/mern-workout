4bc1da3770e8eb34818a90d5a440ec2af2b42c81
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-09-03 08:39:37 +0100

    Add error handling to stop request without all fields being filled sent to the server

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState('');
   const [reps, setReps] = useState('');
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,9 +26,11 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
       setError(null);
+      setEmptyFields([]);
       setTitle('');
       setLoad('');
       setReps('');
@@ -40,13 +43,28 @@ const WorkoutForm = () => {
       <h3>Add a New Workout</h3>
 
       <label>Excersize Title:</label>
-      <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} />
+      <input
+        type='text'
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
+      />
 
       <label>Load (in kg):</label>
-      <input type='number' onChange={(e) => setLoad(e.target.value)} value={load} />
+      <input
+        type='number'
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
+      />
 
       <label>Number of Reps:</label>
-      <input type='number' onChange={(e) => setReps(e.target.value)} value={reps} />
+      <input
+        type='number'
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
+      />
 
       <button>Add Workout</button>
       {error && <div className='error'>{error}</div>}
