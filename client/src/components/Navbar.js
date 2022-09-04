f92b782e51d78b531287b427ab0010e15f5743ea
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-09-04 13:51:43 +0100

    Add logout button and uselogout hook in Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 
+import { useLogout } from '../hooks/useLogout';
+
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className='container'>
         <Link to='/'>
-          <h1>MERN Workout</h1>
+          <h1>Mern Workout</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to='/login'>Login</Link>
             <Link to='/signup'>Signup</Link>
