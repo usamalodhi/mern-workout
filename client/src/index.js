4eec96d6175af514b1ba746e60b11a97a92cf064
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-09-04 09:41:26 +0100

    Add AuthContextProvider wrapper around root app component

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <WorkoutsContextProvider>
-        <App />
-      </WorkoutsContextProvider>
+      <AuthContextProvider>
+        <WorkoutsContextProvider>
+          <App />
+        </WorkoutsContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
